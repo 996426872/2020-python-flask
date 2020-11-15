c31abbe421c2f99135a1c9fbc06dde112cc27a79
yangli <996426872@qq.com>
2020-11-15 17:21:26 +0800

    day6

---
+++ demos/p6.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+
+
+fp = open("C:\\Users\\hacker\\PycharmProjects\\2020-python-flask\\demos\\demo1.txt", 'a+')
+li = 0
+# print(fp.readline())
+# for line in fp:
+#     print(line)
+#     li += 1
+#     if li == 267:
+#         print(li)
+#         break
+
+fp.write("123")
+fp.close()
